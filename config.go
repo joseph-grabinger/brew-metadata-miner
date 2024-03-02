42de708f4c8e8c668774a3eb86316d7c62a963cc
joseph-grabinger <joseph.grabinger@yahoo.de>
2024-03-02 15:58:15 +0100

    Add clone functionality

--- config.go
+++ config.go
@@ -27,12 +27,39 @@ type Config struct {
 	} `yaml:"core_repo"`
 }
 
+// Print prints the configuration to the console.
 func (c *Config) Print() {
 	fmt.Printf("OutputDir: %s\n", c.OutputDir)
 	fmt.Printf("CoreRepo.URL: %s\n", c.CoreRepo.URL)
 	fmt.Printf("CoreRepo.Branch: %s\n", c.CoreRepo.Branch)
 	fmt.Printf("CoreRepo.Dir: %s\n", c.CoreRepo.Dir)
 	fmt.Printf("CoreRepo.Clone: %t\n", c.CoreRepo.Clone)
+}
+
+// Validate validates the configuration and creates directories if needed.
+func (c *Config) Validate() error {
+	// check if the output directory exists
+	if s, err := os.Stat(c.OutputDir); err != nil || s.IsDir() {
+		// create the output directory
+		err = os.Mkdir(c.OutputDir, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	// TODO: add validation of for the core repository directory dependent on clone flag
+
+	// verify the core repository URL is not empty
+	if c.CoreRepo.URL == "" {
+		return fmt.Errorf("the core repository URL is empty")
+	}
+
+	// verify the core repository branch is not empty
+	if c.CoreRepo.Branch == "" {
+		return fmt.Errorf("the core repository branch is empty")
+	}
+
+	return nil
 }
 
 // NewConfig returns a new decoded Config struct from a given configPath.
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+
+	git "gopkg.in/src-d/go-git.v4"
 )
 
 func main() {
@@ -14,4 +17,25 @@ func main() {
 	fmt.Println("Successfully parsed the configuration file:")
 	config.Print()
 
+	if err := config.Validate(); err != nil {
+		log.Fatal(err)
+	}
+
+	if config.CoreRepo.Clone {
+		// err := os.MkdirAll(config.CoreRepo.Dir, 0755)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// clone the core repository
+		_, err = git.PlainClone(config.CoreRepo.Dir, false, &git.CloneOptions{
+			URL: config.CoreRepo.URL,
+			//ReferenceName: plumbing.ReferenceName(config.CoreRepo.Branch),
+			Progress: os.Stdout,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Successfully cloned the core repository")
 }
